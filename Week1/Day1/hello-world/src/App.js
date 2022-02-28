7a0e491f69615e436ff0d89a6c5e803356075e05
rohan <kumawatrohan@gmail.com>
2022-02-28 11:24:40 +0530

    Second Day

--- Week1/Day1/hello-world/src/App.js
+++ Week1/Day1/hello-world/src/App.js
@@ -2,11 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 // import Greet from './components/Greet'
 import { Greet } from './components/Greet'
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
 
 function App() {
   return (
     <div className="App">
-      <Greet></Greet>
+      {/* <Greet></Greet> */}
+      {/* <Welcome></Welcome> */}
+      {/* <Hello /> */}
+      <Greet />
+      <Greet />
+      <Greet />
     </div>
   );
 }
